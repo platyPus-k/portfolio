d9cf540603f68021eb493aa9c5df45146e5413ee
Kyousuke Takaya <platypus.takaya@gmail.com>
2020-07-10 21:28:23 +0900

    パスワードのハッシュ化

--- wordNote/Pages/Users/Create.cshtml.cs
+++ wordNote/Pages/Users/Create.cshtml.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -24,7 +26,7 @@ namespace wordNote.Pages.Users
             {
                 return LocalRedirect(Url.Content("~/"));
             }
-              
+
             return Page();
         }
 
@@ -40,10 +42,53 @@ namespace wordNote.Pages.Users
                 return Page();
             }
 
-            _context.User.Add(user);
+            (string hashed, byte[] salt) = HashPassword(user.Password);
+
+            var masterUser = new User
+            {
+                Id = user.Id,
+                Password = hashed,
+                Name = user.Name,
+                Age = user.Age,
+                Salt = salt
+            };
+
+            bool userCheck = VerifyPassword(hashed, user.Password, salt);
+            if (!userCheck) return Page();
+
+            _context.User.Add(masterUser);
             await _context.SaveChangesAsync();
 
             return LocalRedirect(Url.Content("~/Account/Login"));
         }
+
+        public (string hashedPassword, byte[] salt) HashPassword(string rawPassword)
+        {
+            byte[] salt = GetSalt();
+            string hashed = HashPassword(rawPassword, salt);
+            return (hashed, salt);
+        }
+
+        private string HashPassword(string rawPassword, byte[] salt) =>
+          Convert.ToBase64String(
+            KeyDerivation.Pbkdf2(
+          password: rawPassword,
+          salt: salt,
+          prf: KeyDerivationPrf.HMACSHA512,
+          iterationCount: 10000,
+          numBytesRequested: 256 / 8));
+
+        private byte[] GetSalt()
+        {
+            using (var gen = RandomNumberGenerator.Create())
+            {
+                var salt = new byte[128 / 8];
+                gen.GetBytes(salt);
+                return salt;
+            }
+        }
+
+        public bool VerifyPassword(string hashedPassword, string rawPassword, byte[] salt) =>
+        hashedPassword == HashPassword(rawPassword, salt);
     }
 }
---
+++ wordNote/wwwroot/lib/IPasswordService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace wordNote.wwwroot.lib
+{
+    public class IPasswordService
+    {
+        public interface IPasswordService
+        {
+            bool VerifyPassword(string hashedPassword, string rawPassword, byte[] salt);
+            (string hashedPassword, byte[] salt) HashPassword(string rawPassword);
+        }
+    }
+}
